139cc9aa9151cf2c983188b0d2126fc7c5d2dc03
BrandsDev <lucasbrandao392@gmail.com>
2024-12-31 17:49:19 -0300

    Tentando ajustar o middleware

--- app/middleware.php
+++ app/middleware.php
@@ -19,8 +19,9 @@ return function (App $app) {
         "ignore" => ["/v1/token"],
         "secret" => $app->getContainer()->get(SettingsInterface::class)->get('secretKey'),
         "algorithm" => ["HS256"],
-        "error" => function ($res, $args) {
+        "error" => function ($res, $args) use ($app) {
             error_log('Token JWT: ' . json_encode($args));
+            error_log('Secret Key: ' . $app->getContainer()->get(SettingsInterface::class)->get('secretKey'));
             $data = [
                 "status" => "Error",
                 "message" => $args["message"] . ' Defina um token para acessar a API'
@@ -32,10 +33,13 @@ return function (App $app) {
     ]));
     
     $app->add(function (Request $request, RequestHandler $handler): Response {
+        if ($request->getMethod() === 'OPTIONS') {
+            return $handler->handle($request);
+        }
         $response = $handler->handle($request);
         return $response
             ->withHeader('Access-Control-Allow-Origin', '*')
-            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Headers', 'Authorization, X-Requested-With, Content-Type, Accept, Origin')
             ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     });
     
