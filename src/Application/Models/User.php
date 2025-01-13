9abdee6fbf77dd7d59a420160ef24a43db7b9180
BrandsDev <lucasbrandao392@gmail.com>
2025-01-12 21:18:39 -0300

    Ajsutando a rota para que funcione corretamente e com o token no cadastro

--- src/Application/Models/User.php
+++ src/Application/Models/User.php
@@ -1,15 +1,27 @@
 <?php
 
 namespace App\Application\Models;
+
+use App\Application\Settings\SettingsInterface;
+use Firebase\JWT\JWT;
 use Illuminate\Database\Eloquent\Model;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Container\ContainerInterface;
 
 Class User extends Model{
 
     protected $fillable = [
         "id", "nome", "descricao", "email", "cpf", "celular", "genero", "senha", "url_foto_perfil", "token", "created_at", "updated_at",
     ];
+
+    private ContainerInterface $container;
+    
+    // Adicione o construtor para injetar o container
+    public function __construct(ContainerInterface $container) {
+        parent::__construct();
+        $this->container = $container;
+    }
     
     public function auth(Request $req, Response $res) {
 
@@ -37,7 +49,8 @@ Class User extends Model{
                 'status' => 'sucesso',
                 'message' => 'Login realizado com sucesso',
                 'auth' => true,
-                'id_user' => $user->id,
+                'id' => $user->id,
+                'token' => $user->token,
             ]));
             return $res->withHeader('Content-Type', 'application/json');
         }
@@ -56,9 +69,29 @@ Class User extends Model{
         return $res->withHeader('Content-Type', 'application/json');
     }
 
-    public function addUser(Request $req, Response $res){
+    public function addUser(Request $req, Response $res) {
         $dados = $req->getParsedBody();
-        $user = User::created($dados);
+
+        $settings = $this->container->get(SettingsInterface::class);
+        $secretKey = $settings->get('secretKey');
+
+        $payload = [
+            'email' => $dados->email,
+            'iat' => time(), // Data de emissão
+            // 'exp' => time() + 3600, 
+        ];
+        $token = JWT::encode($payload, $secretKey, 'HS256');
+
+        $insert = [
+            'nome' => $dados->nome,
+            'email' => $dados->email,
+            'cpf' => $dados->cpf,
+            'celular' => $dados->celular,
+            'senha' => $dados->senha,
+            'token' => $token
+        ];
+
+        $user = User::create($insert);
         $res->getBody()->write(json_encode($user));
         return $res->withHeader('Content-Type', 'application/json');
 
